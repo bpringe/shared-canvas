c06cf9102fdfd9b456c849eed8e9a5207ddbbe47
Brandon Ringe <brandon.ringe@gmail.com>
2018-10-24 15:22:49 -0500

    Initial commit. Server running and accepting, printing, and echoing websocket messages

---
+++ src/shared_canvas_server/core.clj
@@ -0,0 +1,22 @@
+(ns shared-canvas-server.core
+  (:gen-class)
+  (:require [org.httpkit.server :refer [with-channel run-server on-close on-receive send!]]
+            [compojure.core :refer [GET defroutes]]
+            [compojure.route :refer [not-found]]))
+
+(defn message-handler
+  [request]
+  (with-channel request channel
+    (on-close channel (fn [status] (println "channel closed:" status)))
+    (on-receive channel (fn [data] (do (println "Received:" data)
+                                       (send! channel data))))))
+
+(defroutes app
+  (GET "/" [] "Hello from Compojure!")
+  (GET "/ws" [] message-handler)
+  (not-found "Not found"))
+
+(defn -main
+  [& args]
+  (run-server app {:port 8000})
+  (println "Server running on port 8080"))
