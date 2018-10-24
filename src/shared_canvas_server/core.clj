74210557ae40bae08b1286b1bb57eedef7a6f129
Brandon Ringe <brandon.ringe@gmail.com>
2018-10-24 16:37:05 -0500

    Add draw events to atom. Send them to new users.

--- src/shared_canvas_server/core.clj
+++ src/shared_canvas_server/core.clj
@@ -2,21 +2,44 @@
   (:gen-class)
   (:require [org.httpkit.server :refer [with-channel run-server on-close on-receive send!]]
             [compojure.core :refer [GET defroutes]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [clojure.data.json :as json]))
 
-(defn message-handler
+(defonce canvas-events (atom []))
+
+(defn handle-message
+  [message]
+  (println "Received:" message)
+  (swap! canvas-events conj (json/read-str message)))
+
+(defn websocket-handler
   [request]
   (with-channel request channel
+    (println "New user connected")
+    (println "Sending canvas events")
+    (send! channel (json/write-str @canvas-events))
     (on-close channel (fn [status] (println "channel closed:" status)))
-    (on-receive channel (fn [data] (do (println "Received:" data)
-                                       (send! channel data))))))
+    (on-receive channel handle-message)))
 
 (defroutes app
   (GET "/" [] "Web server running.")
-  (GET "/ws" [] message-handler)
+  (GET "/ws" [] websocket-handler)
   (not-found "Not found"))
+
+;;;; Server
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(defn start-server
+  [port]
+  (reset! server (run-server #'app {:port port})))
 
 (defn -main
   [& args]
-  (run-server app {:port 8000})
-  (println "Server running on port 8080"))
+  (start-server 8000)
+  (println "Server running on port 8000"))
